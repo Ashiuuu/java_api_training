f8552c56bbc59c60b8c65e83e56e9af724978420
Ashiu <clement.milisavljevic@epita.fr>
2021-06-28 20:49:24 +0200

    Client setup

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -4,27 +4,62 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.IntToDoubleFunction;
 
 public class Launcher
 {
     public static void main(String[] args)
     {
-        try
+        if (args.length == 1)
         {
-            ExecutorService executor = Executors.newFixedThreadPool(1);
-            HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
-            server.createContext("/ping", new PingHandler());
-            server.setExecutor(executor);
-            server.start();
+            try {
+                ExecutorService executor = Executors.newFixedThreadPool(1);
+                HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
+                server.createContext("/ping", new PingHandler());
+                server.createContext("/api/game/start", new StartHandler());
+                server.setExecutor(executor);
+                server.start();
+            } catch (IOException e) {
+                System.out.println("Exception occurred");
+            }
         }
-        catch (IOException e)
+        else if (args.length == 2)
         {
-            System.out.println("Exception occurred");
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(args[1] + "/api/game/start"))
+                .setHeader("Accept", "application/json")
+                .setHeader("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + args[0] + "\", \"message\":\"hello\"}"))
+                .build();
+            try
+            {
+                HttpResponse<String> r = client.send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println(r.statusCode());
+                System.out.println(r.body());
+            }
+            catch(Exception e)
+            {
+                System.out.println("Could not send");
+            }
+        }
+        else
+        {
+
         }
     }
 }
---
+++ src/main/java/fr/lernejo/navy_battle/StartHandler.java
@@ -0,0 +1,65 @@
+package fr.lernejo.navy_battle;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+
+public class StartHandler implements HttpHandler
+{
+    @Override
+    public void handle(HttpExchange t) throws IOException
+    {
+        if (t.getRequestMethod().equals("POST"))
+        {
+            String request = stream_to_string(t.getRequestBody());
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jnode = mapper.readTree(request);
+            String id = jnode.get("id").asText();
+            String url = jnode.get("url").asText();
+            String message = jnode.get("message").asText();
+
+            System.out.println("[" + id + "](" + url + "): " + message);
+            // TODO : add check to see if request is good
+
+            String body = "Accepted";
+            t.sendResponseHeaders(202, body.length());
+            try (OutputStream os = t.getResponseBody())
+            {
+                os.write(body.getBytes());
+            }
+        }
+        else if (t.getRequestMethod().equals("GET"))
+        {
+
+        }
+        else
+        {
+            String body = "Not Found";
+            t.sendResponseHeaders(404, body.length());
+            try (OutputStream os = t.getResponseBody())
+            {
+                os.write(body.getBytes());
+            }
+        }
+    }
+
+    public String stream_to_string(InputStream s) throws IOException
+    {
+        InputStreamReader isr = new InputStreamReader(s, "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+
+        int b;
+        StringBuilder buf = new StringBuilder();
+        while ((b = br.read()) != -1)
+        {
+            buf.append((char) b);
+        }
+
+        br.close();
+        isr.close();
+        return buf.toString();
+    }
+}
